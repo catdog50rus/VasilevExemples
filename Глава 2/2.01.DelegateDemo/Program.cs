9e9a3a9eb76d4bc3efe2b6d7f025e1146cff08d8
catdog50rus <catdog50rus@gmail.com>
2019-12-26 17:19:56 +0300

    1

---
+++ Глава 2/2.01.DelegateDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2._01.DelegateDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
